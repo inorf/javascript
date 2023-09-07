ba9533d7b7252af7f167166d4f72a45beff125d5
inorf <113130007+inorf@users.noreply.github.com>
2023-09-07 09:13:40 +0200

    Update dylevich_zewnetrzny.js

--- dylevich_zewnetrzny.js
+++ dylevich_zewnetrzny.js
@@ -1,2 +1,2 @@
-document.write("ostatnia modyfikacja strony".fontcolor("fuchsia").bold().fontsize(7).overline()+"<br>");
+document.write("ostatnia modyfikacja strony".fontcolor("fuchsia").bold().fontsize(7).text-decoration("line-through")+"<br>");
 document.write(document.lastModified);
